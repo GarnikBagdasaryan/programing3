74b58a68d0032fc7f07bed3dc23c0e6b2c4afefe
GarnikBagdasaryan <garnik.b.g@tumo.org>
2022-02-11 16:20:49 +0400

    chanes

---
+++ programing_3/script.js
@@ -0,0 +1,147 @@
+function generator(matLen, gr, grEat, pr, sk, np) {
+    let matrix = [];
+    for (let i = 0; i < matLen; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < matLen; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 1;
+        }
+    }
+    for (let i = 0; i < grEat; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 2;
+        }
+
+    }
+    for (let i = 0; i < pr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 3;
+        }
+    } for (let i = 0; i < sk; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 4;
+        }
+    }
+    for (let i = 0; i < np; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 5;
+        }
+        return matrix;
+    }
+}
+let side = 20;
+
+let matrix = generator(25, 100, 25, 25, 10, 25);
+
+var grassArr = [];
+var grassEaterArr = [];
+var PredatorArr = [];
+var SunkArr = [];
+var NapastakArr = [];
+function setup() {
+    frameRate(5);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#acacac');
+    for (var y = 0; y < matrix.length; ++y) {
+        for (var x = 0; x < matrix[y].length; ++x) {
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y,);
+                grassArr.push(gr);
+            }
+            else if (matrix[y][x] == 2) {
+                var grEat = new GrassEater(x, y);
+                grassEaterArr.push(grEat);
+            }
+            else if (matrix[y][x] == 3) {
+                var pr = new Predator(x, y);
+                PredatorArr.push(pr);
+            }
+            else if (matrix[y][x] == 4) {
+                var sk = new Sunk(x, y);
+                SunkArr.push(sk);
+            } else if (matrix[y][x] == 5) {
+                var np = new Napastak(x, y);
+                NapastakArr.push(np);
+            }
+            else if (matrix[y][x] == 8) {
+
+            }
+        }
+    }
+
+
+
+}
+
+
+
+function draw() {
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill("green");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 0) {
+                fill("#acacac");
+                rect(x * side, y * side, side, side);
+            } else if (matrix[y][x] == 2) {
+                fill("yellow");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 3) {
+                fill("red");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 4) {
+                fill("blue");
+                rect(x * side, y * side, side, side);
+            } else if (matrix[y][x] == 5) {
+                fill("orange");
+                rect(x * side, y * side, side, side);
+            }
+        }
+    }
+
+
+
+
+
+    for (var i in grassArr) {
+        grassArr[i].mul();
+
+    }
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].mul();
+        grassEaterArr[i].eat();
+    }
+    for (var i in PredatorArr) {
+        PredatorArr[i].mul();
+        PredatorArr[i].eat();
+
+    }
+    for (var i in SunkArr) {
+     SunkArr[i].mul();
+   }
+   for (var i in NapastakArr){
+       NapastakArr[i].mul();
+       NapastakArr[i].eat();
+   }
+
+}
